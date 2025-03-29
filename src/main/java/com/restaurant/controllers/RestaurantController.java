79837f9cea6f34a3616106d14b2685cbd4bd0b34
yassineraddaoui <yassineraddaouicontact@gmail.com>
2025-03-29 06:39:01 +0100

    filter works fine

--- src/main/java/com/restaurant/controllers/RestaurantController.java
+++ src/main/java/com/restaurant/controllers/RestaurantController.java
@@ -3,16 +3,16 @@ package com.restaurant.controllers;
 import com.restaurant.domain.RestaurantCreateUpdateRequest;
 import com.restaurant.domain.dtos.RestaurantCreateUpdateRequestDto;
 import com.restaurant.domain.dtos.RestaurantDto;
+import com.restaurant.domain.dtos.RestaurantSummaryDto;
 import com.restaurant.domain.entities.Restaurant;
 import com.restaurant.mappers.RestaurantMapper;
 import com.restaurant.services.RestaurantService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/restaurants")
@@ -33,4 +33,45 @@ public class RestaurantController {
         RestaurantDto createdRestaurantDto = restaurantMapper.toRestaurantDto(restaurant);
         return ResponseEntity.ok(createdRestaurantDto);
     }
+
+    @GetMapping("/filter")
+    public Page<RestaurantSummaryDto> searchRestaurants(
+            @RequestParam(required = false) String cuisineType,
+            @RequestParam(required = false) Float minRating,
+            @RequestParam(required = false) Double latitude,
+            @RequestParam(required = false) Double longitude,
+            @RequestParam(required = false) Double maxDistanceKm,
+            @RequestParam(defaultValue = "false") boolean filterOpenNow,
+            @RequestParam(defaultValue = "false") boolean requirePhotos,
+            @RequestParam(required = false) String createdById,
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "20") int size
+    ) {
+        PageRequest pageRequest = PageRequest.of(Math.max(0, page - 1), size);
+
+        Page<Restaurant> searchResults = restaurantService.searchRestaurants(
+                pageRequest,
+                cuisineType,
+                minRating,
+                latitude,
+                longitude,
+                maxDistanceKm,
+                filterOpenNow,
+                requirePhotos,
+                createdById
+        );
+
+        return searchResults.map(restaurantMapper::toSummaryDto);
+    }
+
+    @GetMapping
+    public Page<RestaurantSummaryDto> getAllRestaurants(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "20") int size
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        return restaurantService.getAllRestaurants(pageRequest)
+                .map(restaurantMapper::toSummaryDto);
+    }
+
 }
--- src/main/java/com/restaurant/repositories/RestaurantRepository.java
+++ src/main/java/com/restaurant/repositories/RestaurantRepository.java
@@ -1,11 +1,9 @@
 package com.restaurant.repositories;
 
-
 import com.restaurant.domain.entities.Restaurant;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RestaurantRepository extends ElasticsearchRepository<Restaurant, String> {
-    // TODO: Custom queries
 }
