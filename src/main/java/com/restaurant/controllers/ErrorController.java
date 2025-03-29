76c36a90b8617e967bbf32cd865cde4954b748e3
yassineraddaoui <yassineraddaouicontact@gmail.com>
2025-03-29 05:05:05 +0100

    api for validation error

--- src/main/java/com/restaurant/controllers/ErrorController.java
+++ src/main/java/com/restaurant/controllers/ErrorController.java
@@ -6,14 +6,36 @@ import com.restaurant.exceptions.StorageException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 @RestController
 @ControllerAdvice
 @Slf4j
 public class ErrorController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.error("Caught MethodArgumentNotValidException", ex);
+
+        String errorMessage = ex
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        ErrorDto errorDto = ErrorDto.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(errorMessage)
+                .build();
+
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(StorageException.class)
     public ResponseEntity<ErrorDto> handleStorageException(StorageException ex) {
